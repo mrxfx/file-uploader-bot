f44d9b4967ff60d25451f50e70260310b8167950
Flex Coder <jaychonkar617@gmail.com>
2025-05-31 17:57:45 +0530

    index.js

--- index.js
+++ index.js
@@ -10,29 +10,42 @@ const upload = multer({ limits: { fileSize: 30 * 1024 * 1024 } })
 const storage = {}
 
 bot.start(async (ctx) => {
+  await ctx.telegram.sendChatAction(ctx.chat.id, "typing")
   const name = ctx.from.first_name
   const id = ctx.from.id
-  await ctx.replyWithHTML(`👋<b>Welcome ${name}](tg://user?id=${id}),\n\nI am here to host your file for free. Share me file which should be less than 30 mb</b>`)
+  await ctx.replyWithHTML(
+    `👋<b>Welcome <a href="tg://user?id=${id}">${name}</a>,\n\nI am here to host your file for free. Share me file which should be less than 30 mb</b>`,
+    { reply_to_message_id: ctx.message.message_id }
+  )
 })
 
-bot.on('document', async (ctx) => handleFile(ctx, ctx.message.document.file_id, ctx.message.document.file_name))
-bot.on('video', async (ctx) => handleFile(ctx, ctx.message.video.file_id, "video.mp4"))
-bot.on('photo', async (ctx) => {
-  const photos = ctx.message.photo
-  const file_id = photos[photos.length - 1].file_id
-  handleFile(ctx, file_id, "image.jpg")
-})
-bot.on('animation', async (ctx) => handleFile(ctx, ctx.message.animation.file_id, ctx.message.animation.file_name))
+bot.on(["document", "video", "animation", "photo"], async (ctx) => {
+  await ctx.telegram.sendChatAction(ctx.chat.id, "upload_document")
+  let file_id, file_name
 
-async function handleFile(ctx, file_id, file_name) {
+  if (ctx.message.document) {
+    file_id = ctx.message.document.file_id
+    file_name = ctx.message.document.file_name
+  } else if (ctx.message.video) {
+    file_id = ctx.message.video.file_id
+    file_name = "video.mp4"
+  } else if (ctx.message.animation) {
+    file_id = ctx.message.animation.file_id
+    file_name = ctx.message.animation.file_name || "animation.gif"
+  } else if (ctx.message.photo) {
+    const photo = ctx.message.photo.at(-1)
+    file_id = photo.file_id
+    file_name = "image.jpg"
+  }
+
   const file = await ctx.telegram.getFile(file_id)
   const url = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${file.file_path}`
   const buffer = (await axios.get(url, { responseType: 'arraybuffer' })).data
   const id = randomBytes(8).toString("hex")
   storage[id] = { buffer, name: file_name }
   const link = `https://${process.env.VERCEL_URL}/upload?id=${id}`
-  await ctx.reply(link)
-}
+  await ctx.reply(link, { reply_to_message_id: ctx.message.message_id })
+})
 
 app.use(bot.webhookCallback("/"))
 
@@ -41,10 +54,6 @@ app.get("/upload", (req, res) => {
   if (!file) return res.status(404).send("File not found")
   res.setHeader("Content-Disposition", `attachment; filename="${file.name}"`)
   res.send(file.buffer)
-})
-
-app.get("/", (req, res) => {
-  res.send("Bot is running")
 })
 
 export default app
